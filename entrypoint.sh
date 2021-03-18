e6e63e1877ccbe8a0877a659f2f502a827c6e020
Filip Valder <filip.valder@gmail.com>
2021-03-18 18:35:01 +0100

    Testing using docker + separate prod image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-cd /src
 
-export FLASK_DEBUG=1
 export FLASK_APP=hello.py
-flask run --host=0.0.0.0
+echo "$PROJECT_ENV"
+
+if [ "$PROJECT_ENV" == "dev" ] ; then
+	cd /src
+	export FLASK_DEBUG=1
+	flask run --host=0.0.0.0 &
+	py.test-3 -v tests
+	flake8 muj_prvni_py_projekt hello.py tests
+else
+	cd /app
+	flask run --host=0.0.0.0
+fi
---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+import requests
+
+server = 'http://localhost:5000'
+
+def test_hello_world():
+    r = requests.get(server)
+    assert r.text == 'Hello World 4!'
